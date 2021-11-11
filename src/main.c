f82de27955202f4c6ea56223c21f960146a69e67
Neil Kaus <nkaus@uwaterloo.ca>
2021-11-11 14:28:38 -0500

    testing reed switch input

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 // To run a particular example, you should remove the comment (//) in
 // front of exactly ONE of the following lines:
 
-#define BUTTON_BLINK
+ //#define BUTTON_BLINK
 // #define LIGHT_SCHEDULER
 // #define TIME_RAND
 // #define KEYPAD
@@ -26,6 +26,15 @@
 
 #include "ece198.h"
 
+void DisplaySensor(uint16_t pin);
+
+void DisplaySensor(uint16_t pin){
+    if(!HAL_GPIO_ReadPin(GPIOB,pin))
+        SerialPutc('X');
+    else
+        SerialPutc('_');
+}
+
 int main(void)
 {
     HAL_Init(); // initialize the Hardware Abstraction Layer
@@ -38,7 +47,7 @@ int main(void)
     __HAL_RCC_GPIOC_CLK_ENABLE(); // enable port C (for the on-board blue pushbutton, for example)
 
     // initialize the pins to be input, output, alternate function, etc...
-
+    InitializePin(GPIOB, GPIO_PIN_10 | GPIO_PIN_4 , GPIO_MODE_INPUT, GPIO_PULLUP, 0);
     InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // on-board LED
 
     // note: the on-board pushbutton is fine with the default values (no internal pull-up resistor
@@ -48,6 +57,13 @@ int main(void)
     // (anything we write to the serial port will appear in the terminal (i.e. serial monitor) in VSCode)
 
     SerialSetup(9600);
+    SerialPuts("\r\n\n");
+
+    while(1){
+        DisplaySensor(GPIO_PIN_10);
+        DisplaySensor(GPIO_PIN_4);
+        SerialPuts("  \r");
+    }
 
     // as mentioned above, only one of the following code sections will be used
     // (depending on which of the #define statements at the top of this file has been uncommented)
@@ -63,7 +79,7 @@ int main(void)
     while (1) // loop forever, blinking the LED
     {
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        HAL_Delay(250);  // 250 milliseconds == 1/4 second
+        HAL_Delay(1000);  // 250 milliseconds == 1/4 second
     }
 #endif
 
