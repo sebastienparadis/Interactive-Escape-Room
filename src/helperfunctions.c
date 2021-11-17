7cb7497cfab94514426ed1a9457958a14900abf6
Sebastien Paradis <s3paradi@uwaterloo.ca>
2021-11-16 20:57:56 -0500

    7-segment display helper function

--- src/helperfunctions.c
+++ src/helperfunctions.c
@@ -1,15 +1,10 @@
 #include <stdbool.h>
 #include "helperfunctions.h"
-// SIDES OF BOARD
-// Defining the LED ranges and their board sides
-
-
-
-
-
-
 
 // Read Reed Switches
+// Assigning and returning a corresponding integer value to each reed switch
+// When checking if the proper location has been placed, checking to see if the sum of all activated reed switches is appropriate
+
 
 // First Location, "M" --> GPIO PIN B 10
 // Second Location, "O" --> GPIO PIN B 4
@@ -22,25 +17,35 @@ int ReadReed()
     int output;
     output = 0;
     //Check if the first reed switch is activated
+
     if (!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10))
     {
         output += 0b10000;
     }
     
+    //Check if the second reed switch is activated
+
     if (!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4))
     {
         output += 0b1000;
     }
+
+    //Check if the third reed switch is activated
+
 
     if (!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5))
     {
         output += 0b100;
     }
     
+    //Check if the fourth reed switch is activated
+
     if (!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_3))
     {
         output += 0b10;
     }    
+
+    //Check if the fifth reed switch is activated
 
     if (!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10))
     {
@@ -88,23 +93,68 @@ int ReadJoystick(ADC_HandleTypeDef *adc)
 
 // Printing the Next Location on the 7-Segment Display
 
+//GPIO Pin layout: 
+/*
+Segment A (TOP) --> B8 --> GPIOB, GPIO_PIN_8;
+Segment B (UPPER RIGHT) --> B9 --> GPIOB, GPIO_PIN_9;
+Segment C (LOWER RIGHT) --> A5 --> GPIOA, GPIO_PIN_5;
+Segment D (BOTTOM) --> A6 --> GPIOA, GPIO_PIN_6;
+Segment E (LOWER LEFT) --> A7 --> GPIOA, GPIO_PIN_7;
+Segment F (UPPER LEFT) --> B6 --> GPIOB, GPIO_PIN_6;
+Segment G (MIDDLE) --> C7 --> GPIOC, GPIO_PIN_7;
+
+
+*/
+
 void PrintLocation(char n)
 {
    
     if (n = 'O')
     {
-        //print 0
+        //print O
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, true); //Top
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, true); //Upper Right
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, true); //Lower Right
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, true); //Bottom
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, true); //Lower Left
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, true); //Upper Left
+
+        
     }
     if (n = 'R')
     {
         //print R
+        // Print a 0 without the bottom segment
+        // all segments except D
+
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, true); //Top
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, true); //Upper Right
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, true); //Lower Right
+        HAL_GPIO_WritePin( GPIOA, GPIO_PIN_7, true); //Lower Left
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, true); //Upper Left
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, true); //Middle
     }
     if (n = 'S')
     {
         //print 5
+        
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, true); //Top
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, true); //Lower Right
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, true); //Bottom
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, true); //Upper Left
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, true); //Middle
+        
     }
     if (n = 'E')
     {
         //print E
+
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, true); //Top
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, true); //Bottom
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, true); //Lower Left
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, true); //Upper Left
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, true); //Middle
+
     }
 }
+
--- src/helperfunctions.h
+++ src/helperfunctions.h
@@ -3,7 +3,10 @@
 
 
 int ReadReed();
+
 int ReadJoystick(ADC_HandleTypeDef *adc);
+
 uint16_t ReadPhotoResistor();
 //uint16_t CheckPhotoResistor(uint16_t RoomBrightness);
+
 void PrintLocation(char n);
